848b581e931df7e7f4df8fa4e7f4912ef5ca22b7
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-04-22 23:46:35 +0100

    Added JS to weather aspects

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function displayTemperature(response) {
+    console.log(response);
+    console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "b38034b76f133ca05fd67538c5b8c748";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
