785838a123a251c5e8c7b0f607f33a1257f2dca3
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-04-23 12:49:26 +0100

    Created Search function

--- src/app.js
+++ src/app.js
@@ -43,9 +43,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "b38034b76f133ca05fd67538c5b8c748";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+function search(city) {
+    let apiKey = "b38034b76f133ca05fd67538c5b8c748";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
